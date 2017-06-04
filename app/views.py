d80ba10667a109d6c5b78193e4f6a65ef2907504
aditya03011997 <am438@snu.edu.in>
2017-06-04 20:28:14 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import make_response, url_for, request, render_template, flash, session, redirect,session
+from flask import make_response, url_for, request, render_template, flash, session, redirect,session,jsonify
 from flask_oauth import OAuth
 from .forms import ChangeNickForm, ChangePasswordForm, SearchForm, SignupForm, SigninForm, PostForm, RecoveryForm, NewpasswordForm
 from app import authomatic, page, db, models
