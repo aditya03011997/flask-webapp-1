680a41a5ecc583f95bac22d921feb2abc0b29ff1
aditya03011997 <am438@snu.edu.in>
2017-06-04 17:59:02 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -17,24 +17,6 @@ from readability import Document
 # We must configure these 3 values from Google APIs console
 # https://code.google.com/apis/console
 
-GOOGLE_CLIENT_ID = '985983038574-uevth6gnpbaa5r1vpigak0o4i5oiki23.apps.googleusercontent.com'
-GOOGLE_CLIENT_SECRET = 'iC7DTnvR_ZtLh9EtJggG8Ims'
-REDIRECT_URI = '/oauth2callback'  # one of the Redirect URIs from Google APIs console
-
-oauth = OAuth()
- 
-google = oauth.remote_app('google',
-                          base_url='https://www.google.com/accounts/',
-                          authorize_url='https://accounts.google.com/o/oauth2/auth',
-                          request_token_url=None,
-                          request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
-                                                'response_type': 'code'},
-                          access_token_url='https://accounts.google.com/o/oauth2/token',
-                          access_token_method='POST',
-                          access_token_params={'grant_type': 'authorization_code'},
-                          consumer_key=GOOGLE_CLIENT_ID,
-                          consumer_secret=GOOGLE_CLIENT_SECRET)
-
 def get_serializer(secret_key=None):
     if secret_key is None:
         secret_key = page.secret_key
@@ -97,48 +79,6 @@ def login(provider_name):
                                user_email=result.user.email,
                                user_id=result.user.id)
     return response
-
-
-@page.route('/')  # Here is the code for getting access token of user and authorizing it with his google account.
-def index_g():
-    access_token = session.get('access_token')
-    if access_token is None:
-        return redirect(url_for('login'))
- 
-    access_token = access_token[0]
-    from urllib2 import Request, urlopen, URLError
- 
-    headers = {'Authorization': 'OAuth '+access_token}
-    req = Request('https://www.googleapis.com/oauth2/v1/userinfo',
-                  None, headers)
-    try:
-        res = urlopen(req)
-    except URLError, e:
-        if e.code == 401:
-            # Unauthorized - bad token
-            session.pop('access_token', None)
-            return redirect(url_for('login'))
-        return res.read()
- 
-    return res.read()
-
-@page.route('/logingoogle')
-def login_with_google():
-    callback=url_for('authorized', _external=True)
-    return google.authorize(callback=callback)
- 
- 
-@page.route(REDIRECT_URI)
-@google.authorized_handler
-def authorized(resp):
-    access_token = resp['access_token']
-    session['access_token'] = access_token, ''
-    return redirect(url_for('index_g'))
- 
- 
-@google.tokengetter
-def get_access_token():
-    return session.get('access_token')
 
 @page.route('/register', methods=['GET', 'POST'])
 def register():
